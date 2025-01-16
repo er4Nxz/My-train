54a69195d9367acbe623e71e746db304faa98dda
erfan <erfan.call.11@gmail.com>
2025-01-16 17:10:40 +0330

    js= let,concat,const,var

---
+++ S-9/js/main.js
@@ -0,0 +1,30 @@
+// alert("salam khosh omadi be web erfan");
+console.log(typeof "erfan-ahmadi");
+console.log("erfan-ahmadi");
+document.write("<h1>erfan ahmadi</h1>");
+document.getElementById("root").innerHTML = "<h1>erfan ahmdi (2)</h1>";
+//variable
+var lastName = "ahmadi";
+var firstName = "erfan";
+console.log(firstName, typeof firstName);
+console.log(lastName, typeof lastName);
+// let
+{
+   let x = "10";
+  {
+    console.log(x);
+  }
+}
+//const
+const x ="100";
+console.log(x);
+//dublecate
+console.log("my name is "+firstName+" and my last name is "+lastName);
+console.log(firstName,lastName);
+//concat template literal `
+console.log(
+    `my name is ${firstName} and my last name is ${lastName}`
+);
+
+
+
