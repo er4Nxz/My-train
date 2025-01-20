b3df53ea4720cf4e1c328d2cde28da279f5b668b
erfan <erfan.call.11@gmail.com>
2025-01-20 21:41:37 +0330

    casting in ts

---
+++ S-10/typescript/main.js
@@ -0,0 +1,11 @@
+// {
+// //casting in ts
+//   let x: number = Number(prompt("enter your age"));
+//   console.log(typeof x, x);
+//   let y: string | null = prompt("fullName");
+//   console.log(typeof y, y);
+//   let test = String(x);
+//   console.log(typeof test, test);
+//   let test_2 = Number(y);
+//   console.log(test_2);
+// }
