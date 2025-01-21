7d304f8141d60708390aa02b267203f88e4b44bb
erfan <erfan.call.11@gmail.com>
2025-01-21 17:41:14 +0330

    create calculator

--- S-10/typescript/main.js
+++ S-10/typescript/main.js
@@ -10,23 +10,46 @@
 //   console.log(test_2);
 // }
 debugger;
-var username = prompt("username");
-var password = Number(prompt("pass"));
-if ((username === "erfan")) {
-    console.log("this username ".concat(username, " its all right"));
-    if ((password === 12345)) {
-        console.log("WELCOM!");
-    }
-    else {
-        console.log("pass is incorect");
-    }
-}
-else {
-    console.log("username is incorect");
-    if (password === 12345) {
-        console.log("pass is correct");
-    }
-    else {
-        console.log("get out of my web site");
-    }
+// let username: string | null = prompt("username");
+// let password: number = Number(prompt("pass"));
+// if ((username === "erfan")) {
+//   console.log(`this username ${username} its all right`);
+//   if ((password === 12345)) {
+//     console.log("WELCOM!");
+//   } else {
+//     console.log("pass is incorect");
+//   }
+// } else {
+//     console.log("username is incorect");
+//     if(password === 12345){
+//         console.log("pass is correct");
+//     }else{
+//         console.log("get out of my web site");
+//     }
+// }
+// if (username === "erfan" && password === 12345) {
+//   console.log("WELCOM...!");
+//   console.log(`your username is ${username} and your pass is ${password}`);
+// } else {
+//   console.log("get out of my web site");
+// }
+//  CALCULATOR
+var num1 = Number(prompt("enter first number"));
+var operator = prompt("enter operator", "+ - * /");
+var num2 = Number(prompt("enter second number"));
+console.log(num1, operator, num2);
+if (operator === "+") {
+  var sum = num1 + num2;
+  console.log(sum);
+} else if (operator === "-") {
+  var sub = num1 - num2;
+  console.log(sub);
+} else if (operator === "*") {
+  var mul = num1 * num2;
+  console.log(mul);
+} else if (operator === "/") {
+  var div = num1 / num2;
+  console.log(div);
+} else {
+  console.log("WTF???");
 }
