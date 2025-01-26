8e45249485d1f9d9594087e97b87d887549095f2
erfan <erfan.call.11@gmail.com>
2025-01-26 17:26:28 +0330

    function calculator

--- S-11/function/main.js
+++ S-11/function/main.js
@@ -1,11 +1,44 @@
 // functional
-prinData();
-function prinData() {
-  for (let t = 1; t <= 10; t++) {
-    for (let i = 1; i <= 10; i++) {
-      document.write(`<h1>${i}-hello-world!</h1>`);
+// prinData();
+// function prinData() {
+//   for (let t = 1; t <= 10; t++) {
+//     for (let i = 1; i <= 10; i++) {
+//       document.write(`<h1>${i}-hello-world!</h1>`);
+//     }
+//     document.write("<hr/>");
+//   }
+// }
+
+//hoisting
+// console.log(x);
+// var x = 10;
+
+// function input
+// function allData(firstName="Not filled",lastName="Not filled",age=0,email="Not filled") {
+//   document.write(`<h3 class='b'>my name is ${firstName} and my lastName is ${lastName} and my age is ${age} and my email is ${email}</h3>`);
+// }
+// allData("siddharth","sharma",20,"siddharth@gmail.com");
+// allData()
+
+//function calculator
+function calculator(x, y, operator) {
+  switch (operator) {
+    case "+": {
+      console.log(`${x}+${y}=${x + y}`);
+      break;
     }
-    document.write("<hr/>");
+    case "-": {
+      console.log(`${x}-${y}=${x - y}`);
+      break;
+    }
+    case "*": {
+      console.log(`${x}*${y}=${x * y}`);
+      break;
+    }
+    case "/": {
+      console.log(`${x}/${y}=${x / y}`);
+      break;
+    }
   }
 }
-
+calculator(10, 10, "*");
