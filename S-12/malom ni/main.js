dc2354b49a8ac018106e7e6ff8dff4f19a08888b
erfan <erfan.call.11@gmail.com>
2025-02-06 12:05:26 +0330

    class (example)=>calculator

--- S-12/malom ni/main.js
+++ S-12/malom ni/main.js
@@ -40,3 +40,75 @@
 // };
 // console.log(laptop.discount(10));
 // console.log(laptop.Available());
+// class (js)
+// class Person {
+//     fullName;
+//     age;
+//     email;
+//     isMarrid;
+//     printData() {
+//       return `${this.fullName} - ${this.age} - ${this.email} - ${this.isMarrid}`;
+//     }
+//   }
+//   let newAcount = new Person();
+//   newAcount.fullName = "John";
+//   newAcount.age = 25;
+//   newAcount.email = "john@gmail.com";
+//   newAcount.isMarrid = true;
+//   document.write(`<h1>${newAcount.printData()}</h1>`);
+//   let newAcount2 = new Person();
+//   newAcount2.fullName = "jams";
+//   newAcount2.age = 30;
+//   newAcount2.email = "jams@gmail.com";
+//   newAcount2.isMarrid = false;
+//   document.write(`<h1>${newAcount2.printData()}</h1>`);
+// class (counstructor)
+// class Person {
+//     fullName;
+//     age;
+//     email;
+//     isMarrid;
+//     constructor() {
+//       this.fullName = String(prompt("name"));
+//       this.age = Number(prompt("age"));
+//       this.email = String(prompt("email"));
+//       this.isMarrid = Boolean(prompt("is marrid"));
+//     }
+//     printData() {
+//       return `${this.fullName} - ${this.age} - ${this.email} - ${this.isMarrid}`;
+//     }
+//   }
+//   let newAcount = new Person();
+//   document.write(`<h1>${newAcount.printData()}</h1>`);
+//class (example)=>calculator
+// class Calculator {
+//   x;
+//   y;
+//   operand;
+//   constructor(x, y, operand) {
+//     this.x = prompt("number 1...?");
+//     this.y = prompt("number 2...?");
+//     this.operand = prompt("operand...?");
+//   }
+//   add() {
+//     switch (this.operand) {
+//       case "+": {
+//         return `${this.x}+${this.y}=${this.x + this.y}`;
+//       }
+//       case "-": {
+//         return `${this.x}-${this.y}=${this.x - this.y}`;
+//       }
+//       case "*": {
+//         return `${this.x}*${this.y}=${this.x * this.y}`;
+//       }
+//       case "/": {
+//         return `${this.x}/${this.y}=${this.x / this.y}`;
+//       }
+//       default: {
+//         return `fuild`;
+//       }
+//     }
+//   }
+// }
+// let a1 = new Calculator();
+// document.write(`<h1>${a1.add()}</h1>`)
