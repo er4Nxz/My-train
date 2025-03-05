3cf0486691e8ef71211a3e581d80643a7a5fdc72
erfan <erfan.call.11@gmail.com>
2025-03-05 19:12:07 +0330

    creat slider with api

--- S-13/project with api/main.js
+++ S-13/project with api/main.js
@@ -1,4 +1,4 @@
-let Menu = async () => {
+let fetchMenu = async () => {
   let linkApi = await fetch("http://localhost:3000/menu");
   let data = await linkApi.json();
   let menu = data.map((menuItem) => {
@@ -6,6 +6,20 @@ let Menu = async () => {
                 <a href="${menuItem.link}">${menuItem.name}</a>
             </div>`;
   });
-  document.querySelector(".menuNav").insertAdjacentHTML("afterbegin", menu.join())
+  document
+    .querySelector(".menuNav")
+    .insertAdjacentHTML("afterbegin", menu.join());
 };
-Menu()
+fetchMenu();
+
+let fetchSlider = async () => {
+  let linkApi = await fetch("http://localhost:3000/slider");
+  let data = await linkApi.json();
+  let slider = data.map((sliderItem) => {
+    return `<div class="swiper-slide"><img src="${sliderItem.img}" ></div>`;
+  });
+  document
+    .querySelector(".swiper-wrapper")
+    .insertAdjacentHTML("beforeend", slider.join());
+};
+fetchSlider();
