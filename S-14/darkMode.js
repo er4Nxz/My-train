0b65aa8d8984de6467339d58e37c6fa5762a6a12
erfan <erfan.call.11@gmail.com>
2025-03-10 19:57:48 +0330

    dark Mode

---
+++ S-14/darkMode.js
@@ -0,0 +1,32 @@
+let mainDiv = document.querySelectorAll(".nor");
+let btn = document.querySelector(".btn");
+
+let flag = true;
+function darkmode() {
+  if (flag) {
+    setTimeout(() => {
+      btn.innerHTML = "Light Mode";
+      btn.classList.add("btnLight");
+      mainDiv.forEach((divs) => {
+        divs.parentElement.classList.add("bodyDark");
+        divs.classList.add("divDark");
+        divs.children[1].classList.add("pDark");
+        divs.children[2].classList.add("aDark");
+      });
+      flag = false;
+    }, 500);
+  } else {
+    setTimeout(() => {
+      btn.innerHTML = "Dark Mode";
+      btn.classList.remove("btnLight");
+      mainDiv.forEach((divs) => {
+        divs.parentElement.classList.remove("bodyDark");
+        divs.classList.remove("divDark");
+        divs.children[1].classList.remove("pDark");
+        divs.children[2].classList.remove("aDark");
+      });
+      flag = true;
+    }, 500);
+  }
+}
+export default darkmode;
---
+++ S-14/main.js
@@ -0,0 +1,4 @@
+import darkmode from "./darkMode.js";
+// darkmode
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", darkmode);
