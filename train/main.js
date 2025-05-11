7dbcfe57f0a5c5823eab0104ed11c6d2d50e80be
erfan <erfan.call.11@gmail.com>
2025-05-11 17:53:11 +0330

    train swiper with api

--- train/main.js
+++ train/main.js
@@ -1,28 +1,32 @@
 let fetchStory = async () => {
-  const linkApi = await fetch("http://localhost:3000/story");
-  const response = await linkApi.json();
-  const story = response.map((item) => {
-    return `<div class="storyCard">
-                  <div class="storyPerson"><img src="${item.image}" alt=""></div>
+  let linkApi = await fetch("http://localhost:3000/story");
+  let response = await linkApi.json();
+  let story = response.map((item) => {
+    return `<div class="swiper-slide"><div class="storyCard">
+                  <div class="storyPerson"><img src="${item.imag}" alt=""></div>
                   <div class="storyText"><span>${item.text}</span></div>
-                </div>`;
+                </div></div>`;
   });
   document
     .querySelector(".swiper-wrapper")
     .insertAdjacentHTML("beforeend", story.join(""));
 };
-let JsSwiper = () => {
+let jsSwiper = () => {
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 12.5,
-    spaceBetween: 10,
+    slidesPerView: 13.2,
+    spaceBetween: 5,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 };
-let fetchClear = async () => {
+let init = async () => {
   await fetchStory();
-  JsSwiper();
+  jsSwiper();
 };
-fetchClear();
+init();
