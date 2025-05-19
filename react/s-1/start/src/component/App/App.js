438463ba29fffce0a34d530b1292804e6436b25d
erfan <erfan.call.11@gmail.com>
2025-05-19 16:37:18 +0330

    train component

---
+++ react/s-1/start/src/component/App/App.js
@@ -0,0 +1,12 @@
+import Menu from "../Menu/Menu";
+import Slider from "../Slider/Slider";
+
+const App = () => {
+  return (
+    <div>
+      <Menu />
+      <Slider/>
+    </div>
+  );
+};
+export default App;
