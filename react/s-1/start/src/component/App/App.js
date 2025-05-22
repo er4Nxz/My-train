80694d78842aa4f1f54289d6e8c33b6454f55255
erfan <erfan.call.11@gmail.com>
2025-05-22 18:54:43 +0330

    more react

--- react/s-1/start/src/component/App/App.js
+++ react/s-1/start/src/component/App/App.js
@@ -1,12 +1,31 @@
 import Menu from "../Menu/Menu";
 import Slider from "../Slider/Slider";
+import image from "../../img/opium.jpg";
 
+{
+  /* 
+  components
+  fragment 
+  img
+  comment
+  varible
+  condition=> ternery
+  template literal
+  */
+}
 const App = () => {
+  const age = 230;
   return (
-    <div>
+    <>
+      <h1>App</h1>
       <Menu />
-      <Slider/>
-    </div>
+      <Slider />
+      <img src={image} alt="" width="200" />
+      <img src={require("../../img/opium.jpg")} width="300" />
+      <h1>
+        {age > 20 ? `ok because age is ${age}` : `not ok because age is ${age}`}
+      </h1>
+    </>
   );
 };
 export default App;
