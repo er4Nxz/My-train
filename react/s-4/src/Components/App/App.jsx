83d7e2eb7f3db52c683e9d0e77df6e1ddd47e825
erfan <erfan.call.11@gmail.com>
2025-06-01 18:50:04 +0330

    use state

--- react/s-4/src/Components/App/App.jsx
+++ react/s-4/src/Components/App/App.jsx
@@ -1,6 +1,7 @@
 import Person from "../Person/Person";
 import img_1 from "../../../public/img/opium.jpg";
 import Form from "../Form/Form";
+import { useState } from "react";
 const App = () => {
   {
     /* 
@@ -10,6 +11,10 @@ const App = () => {
         props(object)
         props(object in array)
         form  (select attr = multipate) (checkbox => array filter send to backend)
+        USE STATE
+        attr disabled for input
+        css condtional
+        call back function
     */
   }
   // const firstName = "didi";
@@ -19,15 +24,16 @@ const App = () => {
   //   { fullName: "erfan Ahmadi", age: 18 },
   // ];
 
-  const printValue = (e) => {
-    console.log(e.target.value);
-  };
-  const printValueSelect = (e) => {
-    let multi = [...e.target.selectedOptions]
-      .map((item) => item.value)
-      .toString();
-    console.log(multi);
-  };
+  // const printValue = (e) => {
+  //   console.log(e.target.value);
+  // };
+  // const printValueSelect = (e) => {
+  //   let multi = [...e.target.selectedOptions]
+  //     .map((item) => item.value)
+  //     .toString();
+  //   console.log(multi);
+  // };
+  const [counter, setCounter] = useState(0);
   return (
     <>
       {/* <Person firstName={firstName} print={print} />
@@ -41,7 +47,33 @@ const App = () => {
       })}
         */}
       {/* form 👇👇 */}
-      <Form printValueSelect={printValueSelect} printValue={printValue} />
+      {/* <Form printValueSelect={printValueSelect} printValue={printValue} /> */}
+      <div className="w-1/4 mx-auto">
+        <h1 className="text-center my-[20px] text-4xl">counter:{counter}</h1>
+        <button
+          className="bg-amber-400 p-[5px] px-3 rounded-2xl mx-[2px] cursor-pointer"
+          onClick={() => setCounter(counter + 1)}
+        >
+          +
+        </button>
+        <button
+          className={
+            counter <= 0
+              ? "bg-red-500 p-[5px] px-3 rounded-2xl mx-[2px] cursor-pointer opacity-[0.5]"
+              : "bg-amber-400 p-[5px] px-3 rounded-2xl mx-[2px] cursor-pointer"
+          }
+          onClick={() => setCounter(counter - 1)}
+          disabled={counter <= 0 ? "disabled" : null}
+        >
+          -
+        </button>
+        <button
+          className="bg-amber-400 p-[5px] px-3 rounded-2xl mx-[2px] cursor-pointer"
+          onClick={() => setCounter(0)}
+        >
+          Reset
+        </button>
+      </div>
     </>
   );
 };
