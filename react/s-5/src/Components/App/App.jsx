f868ab4fd17046bf421ee4c766a2e30f006567f9
erfan <erfan.call.11@gmail.com>
2025-06-04 23:31:27 +0330

    more use state / use state in array

---
+++ react/s-5/src/Components/App/App.jsx
@@ -0,0 +1,11 @@
+import ArrayUseState from "../ArrayUseState/ArrayUseState";
+
+
+const App = () => {
+  {
+    /* array in use state and update it */
+  }
+  return (<ArrayUseState />);
+};
+
+export default App;
---
+++ react/s-5/src/Components/ArrayUseState/ArrayUseState.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
+const ArrayUseState = () => {
+  const [person, setPerson] = useState(["Erfan Ahmadi", "Sadaf Yosefi"]);
+  const [name, setName] = useState("");
+  const addToPerson = () => {
+    setPerson([...person, name]);
+    setName("");
+  };
+  const addToName = (e) => {
+    setName(e.target.value);
+  };
+  const printLi = person?.map((item, index) => {
+    return (
+      <li className="list-decimal" key={index + 1}>
+        {item}
+      </li>
+    );
+  });
+
+  const [icon, setIcon] = useState(true);
+  const [type, setType] = useState("password");
+  return (
+    <>
+      {/* list Name */}
+      <div className="mt-2 mx-auto rounded-2xl bg-fuchsia-300 p-3 w-1/3">
+        <label htmlFor="fullName" className="cursor-pointer">
+          full Name :
+        </label>
+        <input
+          type="text"
+          id="fullName"
+          className="bg-blue-300 rounded-lg m-1 p-[2px] text-white"
+          onChange={addToName}
+          value={name}
+        />
+        <button
+          className="bg-cyan-300 rounded-lg cursor-pointer p-1"
+          onClick={addToPerson}
+        >
+          click
+        </button>
+        <ul className="text-center">{printLi}</ul>
+      </div>
+      {/* password */}
+      <div className="w-1/3 bg-cyan-700 p-3 rounded-2xl mt-2 mx-auto flex flex-row justify-center items-center gap-1">
+        <label htmlFor="pssword" className="text-white">
+          password :
+        </label>
+        <input
+          type={type}
+          id="password"
+          className="bg-white rounded-2xl p-1 focus:outline-none"
+        />
+        {icon ? (
+          <FaEyeSlash
+            fontSize={20}
+            color="white"
+            className="cursor-pointer  p-1 w-[30px] transition-all ease-linear h-[30px] hover:bg-white/[0.3] hover:rounded-4xl active:bg-white/[0.7]"
+            onClick={() => {
+              setIcon(!icon), setType("text");
+            }}
+          />
+        ) : (
+          <FaEye
+            fontSize={20}
+            color="white"
+            className="cursor-pointer  p-1 w-[30px] transition-all ease-linear h-[30px] hover:bg-white/[0.3] hover:rounded-4xl active:bg-white/[0.7]"
+            onClick={() => {
+              setIcon(!icon), setType("password");
+            }}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ArrayUseState;
