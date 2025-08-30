f43f4d24d86261e250272e2cbb996b469e1bce47
erfan <erfan.call.11@gmail.com>
2025-08-30 18:42:19 +0330

    Complete parts 1 to 20

--- TS/TS-Train/src/main.ts
+++ TS/TS-Train/src/main.ts
@@ -1,1 +1,38 @@
+// // array 2D
+// let user: [[string, number], [string, number]] = [
+//   ["admin", 1],
+//   ["user", 2],
+// ];
+// user.map((item) => {
+//   item.map((item2) => {
+//     console.log(item2);
+//   });
+// });
 
+// // output and parametr types
+// function isLogin(username: string, password: string): boolean {
+//   if (username === "admin" && password === "admin") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(isLogin("admin", "admin"));
+
+// // union type
+// let test: number | string = 12;
+// console.log(test);
+
+// // literal type
+// let age : 18 = 18
+// age = 20
+// console.log(age);
+
+// // template riteral type
+// function setCssProperty(param: `${number}px` | `${number}%` | `${number}rem`) {
+//   return param;
+// }
+// console.log(setCssProperty("12px"));
+// console.log(setCssProperty("100%"));
+// console.log(setCssProperty("3rem"));
+// console.log(setCssProperty("1rx"));
