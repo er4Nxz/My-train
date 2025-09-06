a58f1075f480472418051d9f9c9289f9ae915672
erfan <erfan.call.11@gmail.com>
2025-09-06 20:38:19 +0330

    ts train

---
+++ TS/TS-Train/src/Episode/E-20-40.ts
@@ -0,0 +1,23 @@
+// // type unknown
+// let test: unknown = 12;
+// test++; // ERROR
+// if (typeof test == "number") {
+//   test++;
+// }
+// console.log(test);
+
+// //type casting
+// let test: unknown = 12;
+// let test2 =  test as string
+
+// // const assertion
+// const number = [1,2,3] as const
+// number.push(10) // ERROR
+// console.log(number);
+
+// Example
+// let number = [10, 20] as const;
+// function add(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+// console.log(add(...number));
