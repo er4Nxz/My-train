d7b9c199c36cb0085bca29af6bb2edbf00afa5fb
erfan <erfan.call.11@gmail.com>
2025-09-10 21:57:22 +0330

    callback/alias type / function type

--- TS/TS-Train/src/Episode/E-20-40.ts
+++ TS/TS-Train/src/Episode/E-20-40.ts
@@ -42,3 +42,46 @@
 // };
 // console.log(user);
 
+// // type function
+// let sum = (num1: number, num2: number): number => {
+//   return num1 + num2;
+// };
+// let logger = (param: void): void => {
+//   console.log(param);
+// };
+// let testFunc: (num1: number, num2: number) => number;
+// testFunc = sum;
+// testFunc = logger; // ERROR
+
+// // callback
+// let testFunc = (
+//   num1: number,
+//   num2: number,
+//   callback: (param: any) => void
+// ): void => {
+//   let result = num1 + num2;
+//   callback(result);
+// };
+
+// testFunc(12, 8, (result) => {
+//   console.log(result);
+// });
+
+// // alias type
+// type bolNum = number | boolean;
+// type numStr = bolNum | string;
+// let item1: numStr = 12;
+// let item2: numStr = 13;
+// let item3: numStr = 14;
+// item1 = true
+// console.log(item1,item2,item3);
+
+// // alias type litteral
+// type Role = "admin" | "User" | "Teacher";
+// let logger = (param: Role): void => {
+//   if (param === "admin" || param === "User" || param === "Teacher") {
+//     console.log(param);
+//   }
+// };
+// logger("Teacher");
+
