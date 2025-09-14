ce5fbedc35853beaa039b54a09d30215d436ae49
erfan <erfan.call.11@gmail.com>
2025-09-14 22:47:03 +0330

    start to learn Object-oriented هد فس

---
+++ TS/TS-Train/src/Episode/E-41-60.ts
@@ -0,0 +1,14 @@
+// // object oriented
+// // class
+// class Person {
+//   name: string;
+//   email: string;
+//   age: number;
+//   constructor(name: string, email: string, age: number) {
+//     this.name = name;
+//     this.email = email;
+//     this.age = age;
+//   }
+// }
+// const erfanPerson = new Person("erfan", "e@e.com", 18);
+// console.log(erfanPerson);
