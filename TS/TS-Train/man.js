fd64d6704388d85ed0c8a844b6a40678984db655
erfan <erfan.call.11@gmail.com>
2025-09-30 23:24:46 +0330

    train ans start ts + react

---
+++ TS/TS-Train/man.js
@@ -0,0 +1,11 @@
+//@ts-check
+/**
+ * this function is plus
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {void}
+ */
+const plus = (num1, num2) => {
+  console.log(num1 + num2);
+};
+export {plus};
